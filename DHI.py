b891d39823f410cbaccb72f979d9cf2f017523bf
Piao Yingchao <yingchao.piao@gmail.com>
2016-06-29 04:24:14 -0700

    2016-06-29 ReadTiff

--- DHI.py
+++ DHI.py
@@ -1,11 +1,22 @@
-import gdal
-import sys
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
-# Read GeoTIFF images
-try:
-    tif = gdal.Open('filename.tif')
-    tifArray = tif.ReadAsArray()
-except:
-    print 'The file does not exist.'
-    sys.exit(0)
-    
+###############################################################################
+
+###############################################################################
+import gdal
+from methods_on_tiff import *
+
+#获取Fpar_1km文件列表
+sharedPath='/mnt/hgfs/Data/qhl'
+result = getRasters(sharedPath, ['Fpar_1km'])
+
+for i in result:
+    if i:
+        try:
+            tif = gdal.Open(i[0])
+            tifArray = tif.ReadAsArray()
+        except:
+            print i[0] + ' does not exist.'
+    else:
+        continue

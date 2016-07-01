b488b3216066c9536ecfd582647e3aa0a01bad1c
Piao Yingchao <yingchao.piao@gmail.com>
2016-07-01 02:02:02 -0700

    2016-07-01  Calculate max_monthly fpar.

--- DHI.py
+++ DHI.py
@@ -4,24 +4,45 @@
 ###############################################################################
 
 ###############################################################################
-import gdal
 from methods_on_tiff import *
 from Convert import *
+import time
 
+st = time.time()
 # 获取Fpar_1km文件列表
 sharedPath = '/mnt/hgfs/Data/qhl'
 result = getRasters(sharedPath, ['Fpar_1km'])
 
+dict = {'year': [], 'month': [], 'filesname': []}
+
 for i in result:
     if i:
-        print i[0]
-        # Find the date of the tiff file
-        date_of_file = date_of_modis(i[0])
-        print date_of_file
-        try:
-            tif = gdal.Open(i[0])
-            tifArray = tif.ReadAsArray()
-        except:
-            print i[0] + ' does not exist.'
+        file_date = date_of_modis(i[0])
+        if not file_date[0] in dict['year']:
+            if not dict['month'] == []:
+                period = str(dict['year'].pop()) + '_' + str(dict['month'].pop())
+                print period
+                print dict['filesname']
+                max_fpar(sharedPath, period, dict['filesname'])
+                dict['year'] = [file_date[0]]
+                dict['month'] = [file_date[1]]
+                dict['filesname'] = i
+            else:
+                dict['year'] = [file_date[0]]
+                dict['month'] = [file_date[1]]
+                dict['filesname'] = i
+        else:
+            if not file_date[1] in dict['month']:
+                period = str(dict['year'][0]) + '_' + str(dict['month'].pop())
+                print period
+                print dict['filesname']
+                max_fpar(sharedPath, period, dict['filesname'])
+                dict['month'] = [file_date[1]]
+                dict['filesname'] = i
+            else:
+                dict['filesname'].append(i[0])
     else:
         continue
+et = time.time()
+print '计算MAX_Montly耗时：' + str(et - st) + ' s'
+
--- methods_on_tiff.py
+++ methods_on_tiff.py
@@ -5,8 +5,9 @@
 ###############################################################################
 
 import os
-import gdal
+from osgeo.gdalnumeric import *
 import numpy as np
+
 
 ##返回形式为 [['MCD15A3A2002185_Fpar_1km_MOD_Grid_MOD15A2.tif'],['MCD15A3A2002189_Fpar_1km_MOD_Grid_MOD15A2.tif']] 的文件索引
 ##参数： folder为根目录，keywords为文件名的关键字
@@ -30,47 +31,62 @@ def getRasters(folder, keywords):
 
     return result
 
-def max_monthly(filesname):
+
+##计算文件序列的最大值
+# 参数： path输出的文件路径，period为monthly/annual，filesname为period内fpar的tiff文件列表
+def max_fpar(path, period, filesname):
     maxArray = []
-    XSize = 0
-    YSize = 0
     for k in filesname:
-        try:
-            tif = gdal.Open(k[0])
+        tif = gdal.Open(k)
+        tifTemp = tif.ReadAsArray()
+        tifArray = np.array(tifTemp)
+        if maxArray == []:
+            maxArray.append(tifArray)
+            maxArray = np.array(maxArray[0])
+
+            # Set outfile info
             band = tif.GetRasterBand(1)
-            XSize = tif.RasterXSize
-            YSize = tif.RasterYSize
-            tifTemp = tif.ReadAsArray()
-            tifArray = np.array(tifTemp)
-            if maxArray == []:
-                maxArray.append(tifArray)
-                maxArray = np.array(maxArray[0])
-            else:
-                maxArray = maxArray * (maxArray >= tifArray) + tifArray * (maxArray < tifArray)
-        except:
-            print k[0] + ' does not exist.'
-    return maxArray, band.DataType, XSize, YSize
+            outfilename = path + '/' + str(period) + '_max.tiff'
+            print outfilename
+            image_format = 'GTiff'
+            driver = gdal.GetDriverByName(image_format)
+            dsOut = driver.Create(outfilename, tif.RasterXSize, tif.RasterYSize, 1, band.DataType)
+            CopyDatasetInfo(tif, dsOut)
+            bandOut = dsOut.GetRasterBand(1)
+
+        else:
+            maxArray = maxArray * (maxArray >= tifArray) + tifArray * (maxArray < tifArray)
+    maxArray.tolist()
+    BandWriteArray(bandOut, maxArray)
+
+##计算文件序列的总和
+# 参数： path输出的文件路径，period为month/annual，filesname为period内fpar的tiff文件列表
+def cum_fpar(path, period, filesname):
+    cumArray = []
+    for k in filesname:
+        tif = gdal.Open(k)
+        tifTemp = tif.ReadAsArray()
+        tifArray = np.array(tifTemp)
+        if cumArray == []:
+            cumArray.append(tifArray)
+            cumArray = np.array(cumArray[0])
+
+            # Set outfile info
+            band = tif.GetRasterBand(1)
+            outfilename = path + '/' + str(period) + '_cum.tiff'
+            image_format = 'GTiff'
+            driver = gdal.GetDriverByName(image_format)
+            dsOut = driver.Create(outfilename, tif.RasterXSize, tif.RasterYSize, 1, band.DataType)
+            CopyDatasetInfo(tif, dsOut)
+            bandOut = dsOut.GetRasterBand(1)
+
+        else:
+            cumArray = cumArray + tifArray
+        cumArray.tolist()
+    BandWriteArray(bandOut, cumArray)
 
 
-
-def write_tiff(outpath, tifArray, dataType, XSize, YSize):
-    dst_filename = outpath + 'test.tiff'
-    image_format = 'GTiff'
-    driver = gdal.GetDriverByName(image_format)
-    dst_ds = driver.Create(dst_filename, XSize, YSize, 1, dataType)
-    dst_ds.GetRasterBand(1).WriteArray(tifArray, 0, 0)
-    dst_ds = None
-
-    return dst_filename
-
-outpath='/mnt/hgfs/Data/qhl/'
-filesname = [['/mnt/hgfs/Data/qhl/MCD15A3.005/MCD15A3A2002185/MCD15A3A2002185_Fpar_1km_MOD_Grid_MOD15A2.tif'],
-             ['/mnt/hgfs/Data/qhl/MCD15A3.005/MCD15A3A2002189/MCD15A3A2002189_Fpar_1km_MOD_Grid_MOD15A2.tif']]
-[a, datatype, x, y] = max_monthly(filesname)
-print a, datatype, x, y
-write_tiff(outpath, a, datatype, x, y)
-
-
-
-
-
+# outpath = '/mnt/hgfs/Data/qhl/'
+# filesname = [['/mnt/hgfs/Data/qhl/MCD15A3.005/MCD15A3A2002185/MCD15A3A2002185_Fpar_1km_MOD_Grid_MOD15A2.tif'],
+#              ['/mnt/hgfs/Data/qhl/MCD15A3.005/MCD15A3A2002189/MCD15A3A2002189_Fpar_1km_MOD_Grid_MOD15A2.tif']]
+# max_fpar(outpath, 'monthly', filesname)

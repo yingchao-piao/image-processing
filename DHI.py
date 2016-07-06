f49d8103430bf233152f1514ca0b28f91997653b
Piao Yingchao <yingchao.piao@gmail.com>
2016-07-06 00:45:56 -0700

    2016-07-06 Update for new fpar data

--- DHI.py
+++ DHI.py
@@ -4,7 +4,8 @@
 
 from methods_on_tiff import *
 import time
-sharedPath = '/mnt/hgfs/Data/qhl'
+# sharedPath = '/mnt/hgfs/Data/qhl'
+
 
 #########################################
 # 计算每月的fpar最大值，减少云雪及其他噪声的影响
@@ -17,6 +18,28 @@ sharedPath = '/mnt/hgfs/Data/qhl'
 # max_monthly_fpar(sharedPath, fpar_files)
 # et = time.time()
 # print '计算MAX_Montly耗时：' + str(et - st) + ' s'
+
+sharedPath = '/mnt/hgfs/Data/Fpar'
+
+#########################################
+# 计算每月的fpar最大值，减少云雪及其他噪声的影响
+# 输出名为2013_9_max.tiff形式的影像文件
+#########################################
+
+# 获取Fpar_1km文件列表
+st = time.time()
+fpar_files = getRasters(sharedPath, ['Fpar_1km'])
+files = []
+temp = []
+for i in range(len(fpar_files)):
+    for k in range(len(fpar_files[i])):
+        temp.append(fpar_files[i][k])
+        files.append(temp)
+        temp = []
+max_monthly_fpar(sharedPath, files)
+et = time.time()
+print '计算MAX_Montly耗时：' + str(et - st) + ' s'
+
 
 #########################################
 # 计算DHI_CUM
@@ -45,20 +68,20 @@ sharedPath = '/mnt/hgfs/Data/qhl'
 # 计算DHI_SEA
 # 输出名为2013_sea.tiff形式的影像文件
 #########################################
-st = time.time()
-cumfile = []
-cum = getRasters(sharedPath, ['cum'])
-for i in cum:
-    if i:
-        cumfile.append(i)
-    else:
-        continue
-for k in cumfile:
-    if k:
-        for n in range(len(k)):
-            year = k[n][-13:-9]
-            DHI_sea(sharedPath, year, k[n])
-    else:
-        continue
-et = time.time()
-print '计算DHI_sea耗时：' + str(et - st) + ' s'
+# st = time.time()
+# cumfile = []
+# cum = getRasters(sharedPath, ['cum'])
+# for i in cum:
+#     if i:
+#         cumfile.append(i)
+#     else:
+#         continue
+# for k in cumfile:
+#     if k:
+#         for n in range(len(k)):
+#             year = k[n][-13:-9]
+#             DHI_sea(sharedPath, year, k[n])
+#     else:
+#         continue
+# et = time.time()
+# print '计算DHI_sea耗时：' + str(et - st) + ' s'
